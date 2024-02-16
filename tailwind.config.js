b5589265f273e069f6f71da3ad013dfc147c080b
Mateus Souza <80061497+mateuseds@users.noreply.github.com>
2024-02-16 11:33:50 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.tsx",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      }
+      /* colors: {
+        rocketseat: '#8257e6'
+      } */
+    },
+  },
+  plugins: [],
+}
+
